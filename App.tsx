f0cc5fc4e8f00bba3c01a4bb0b23f21737a01350
Willian lima <william_slayer@hotmail.com>
2023-01-18 15:26:21 -0300

    Criado componente loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {useFonts,Inter_400Regular, Inter_600SemiBold,Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold,Inter_700Bold, Inter_800ExtraBold
+  });
+
+  if(!fontLoaded) {
+    return (
+    <Loading/>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </View>
   );
 }
@@ -13,11 +24,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text:{
-    color:'green'
+    color:'#fff',
+    fontFamily:"Inter_800ExtraBold"
   }
 });
